98672fa282eaa816b21e11de483017dab4ac1421
Mark Ryan Dasco <mrkrynmdsco@gmail.com>
2020-02-06 00:24:01 +0800

    Tried an idea of generic pointer

--- src/array.h
+++ src/array.h
@@ -3,45 +3,51 @@
 
 #include "types.h"
 
-struct array_cfg {
+typedef struct array {
     uint32 nitem;
     dtype_e dtype;
-};
+    void* data;
+} Array;
 
-typedef struct uint8_array {
-    struct array_cfg cfg;
-    uint8* data;
-} UInt8Array;
+// struct array_cfg {
+//     uint32 nitem;
+//     dtype_e dtype;
+// };
 
-typedef struct uint16_array {
-    struct array_cfg cfg;
-    uint16* data;
-} UInt16Array;
+// typedef struct uint8_array {
+//     struct array_cfg cfg;
+//     uint8* data;
+// } UInt8Array;
 
-typedef struct uint32_array {
-    struct array_cfg cfg;
-    uint32* data;
-} UInt32Array;
+// typedef struct uint16_array {
+//     struct array_cfg cfg;
+//     uint16* data;
+// } UInt16Array;
 
-typedef struct sint8_array {
-    struct array_cfg cfg;
-    sint8* data;
-} Int8Array;
+// typedef struct uint32_array {
+//     struct array_cfg cfg;
+//     uint32* data;
+// } UInt32Array;
 
-typedef struct sint16_array {
-    struct array_cfg cfg;
-    sint16* data;
-} Int16Array;
+// typedef struct sint8_array {
+//     struct array_cfg cfg;
+//     sint8* data;
+// } Int8Array;
 
-typedef struct sint32_array {
-    struct array_cfg cfg;
-    sint32* data;
-} Int32Array;
+// typedef struct sint16_array {
+//     struct array_cfg cfg;
+//     sint16* data;
+// } Int16Array;
 
-typedef struct real32_array {
-    struct array_cfg cfg;
-    real32* data;
-} Real32Array;
+// typedef struct sint32_array {
+//     struct array_cfg cfg;
+//     sint32* data;
+// } Int32Array;
+
+// typedef struct real32_array {
+//     struct array_cfg cfg;
+//     real32* data;
+// } Real32Array;
 
 
 #endif // ARRAY_H
--- test/test_array.c
+++ test/test_array.c
@@ -7,58 +7,70 @@ void setUp(void) {
 void tearDown(void) {
 }
 
-void test_array_UInt8_Initialization_static(void) {
-    UInt8Array arr;
+void test_array_Initialization_static(void) {
+    Array arr;
     TEST_ASSERT_EQUAL(16, sizeof(arr));
     TEST_ASSERT_EQUAL(8, sizeof(arr.data));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
+    TEST_ASSERT_EQUAL(4, sizeof(arr.nitem));
+    TEST_ASSERT_EQUAL(4, sizeof(arr.dtype));
+
+    uint32 x = 0xDEADBEEF;
+    arr.data = &x;
+    TEST_ASSERT_EQUAL(x, *(uint32*)arr.data);
 }
 
-void test_array_UInt16_Initialization_static(void) {
-    UInt16Array arr;
-    TEST_ASSERT_EQUAL(16, sizeof(arr));
-    TEST_ASSERT_EQUAL(8, sizeof(arr.data));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
-}
+// void test_array_UInt8_Initialization_static(void) {
+//     UInt8Array arr;
+//     TEST_ASSERT_EQUAL(16, sizeof(arr));
+//     TEST_ASSERT_EQUAL(8, sizeof(arr.data));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
+// }
 
-void test_array_UInt32_Initialization_static(void) {
-    UInt32Array arr;
-    TEST_ASSERT_EQUAL(16, sizeof(arr));
-    TEST_ASSERT_EQUAL(8, sizeof(arr.data));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
-}
+// void test_array_UInt16_Initialization_static(void) {
+//     UInt16Array arr;
+//     TEST_ASSERT_EQUAL(16, sizeof(arr));
+//     TEST_ASSERT_EQUAL(8, sizeof(arr.data));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
+// }
 
-void test_array_Int8_Initialization_static(void) {
-    Int8Array arr;
-    TEST_ASSERT_EQUAL(16, sizeof(arr));
-    TEST_ASSERT_EQUAL(8, sizeof(arr.data));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
-}
+// void test_array_UInt32_Initialization_static(void) {
+//     UInt32Array arr;
+//     TEST_ASSERT_EQUAL(16, sizeof(arr));
+//     TEST_ASSERT_EQUAL(8, sizeof(arr.data));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
+// }
 
-void test_array_Int16_Initialization_static(void) {
-    Int16Array arr;
-    TEST_ASSERT_EQUAL(16, sizeof(arr));
-    TEST_ASSERT_EQUAL(8, sizeof(arr.data));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
-}
+// void test_array_Int8_Initialization_static(void) {
+//     Int8Array arr;
+//     TEST_ASSERT_EQUAL(16, sizeof(arr));
+//     TEST_ASSERT_EQUAL(8, sizeof(arr.data));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
+// }
 
-void test_array_Int32_Initialization_static(void) {
-    Int32Array arr;
-    TEST_ASSERT_EQUAL(16, sizeof(arr));
-    TEST_ASSERT_EQUAL(8, sizeof(arr.data));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
-}
+// void test_array_Int16_Initialization_static(void) {
+//     Int16Array arr;
+//     TEST_ASSERT_EQUAL(16, sizeof(arr));
+//     TEST_ASSERT_EQUAL(8, sizeof(arr.data));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
+// }
 
-void test_array_Real32_Initialization_static(void) {
-    Real32Array arr;
-    TEST_ASSERT_EQUAL(16, sizeof(arr));
-    TEST_ASSERT_EQUAL(8, sizeof(arr.data));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
-    TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
-}
+// void test_array_Int32_Initialization_static(void) {
+//     Int32Array arr;
+//     TEST_ASSERT_EQUAL(16, sizeof(arr));
+//     TEST_ASSERT_EQUAL(8, sizeof(arr.data));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
+// }
+
+// void test_array_Real32_Initialization_static(void) {
+//     Real32Array arr;
+//     TEST_ASSERT_EQUAL(16, sizeof(arr));
+//     TEST_ASSERT_EQUAL(8, sizeof(arr.data));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.nitem));
+//     TEST_ASSERT_EQUAL(4, sizeof(arr.cfg.dtype));
+// }
