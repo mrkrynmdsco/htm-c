c7859077eea0a3a8073ed4d8dc8c1adf992aca0a
Mark Ryan Dasco <mrkrynmdsco@gmail.com>
2020-02-06 21:03:11 +0800

    Revert array module

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 * This repository contains implementation of HTM objects using C programming language to manage memory size and maximize speed and efficiency.
 * These modules are meant to be compiled into shared library (e.g. *.dll or *.a)
+* Tested on a 64-bit system
--- src/array.h
+++ src/array.h
@@ -3,51 +3,9 @@
 
 #include "types.h"
 
-typedef struct array {
+struct Array {
     uint32 nitem;
     dtype_e dtype;
-    void* data;
-} Array;
-
-// struct array_cfg {
-//     uint32 nitem;
-//     dtype_e dtype;
-// };
-
-// typedef struct uint8_array {
-//     struct array_cfg cfg;
-//     uint8* data;
-// } UInt8Array;
-
-// typedef struct uint16_array {
-//     struct array_cfg cfg;
-//     uint16* data;
-// } UInt16Array;
-
-// typedef struct uint32_array {
-//     struct array_cfg cfg;
-//     uint32* data;
-// } UInt32Array;
-
-// typedef struct sint8_array {
-//     struct array_cfg cfg;
-//     sint8* data;
-// } Int8Array;
-
-// typedef struct sint16_array {
-//     struct array_cfg cfg;
-//     sint16* data;
-// } Int16Array;
-
-// typedef struct sint32_array {
-//     struct array_cfg cfg;
-//     sint32* data;
-// } Int32Array;
-
-// typedef struct real32_array {
-//     struct array_cfg cfg;
-//     real32* data;
-// } Real32Array;
-
+};
 
 #endif // ARRAY_H
