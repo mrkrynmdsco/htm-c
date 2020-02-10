4ec1c2f37ac326f826338402c516bc9604691410
Mark Ryan Dasco <mrkrynmdsco@gmail.com>
2020-02-10 20:57:41 +0800

    Added new data array types

--- src/array.c
+++ src/array.c
@@ -3,27 +3,64 @@
 
 
 void Array_ctor (Array* const self, dtype_e dt, uint32 i) {
-    switch (dt) {
-        case UInt8: self->dtype = dt; break;
-        default:
-            break;
-    }
-
+    self->dtype = dt;
     self->nitem = i;
-
     return;
 }
 
 void* Array_Create (dtype_e dt, uint32 i) {
-    UInt8Array* ptr;
     switch (dt) {
-    case UInt8:
-        ptr = (UInt8Array*) malloc(sizeof(UInt8Array));
-        Array_ctor(&ptr->cfg, UInt8, i);
-        return ptr;
-    
-    default:
-        break;
+        case UInt8: {
+            UInt8Array* ptr;
+            ptr = (UInt8Array*) malloc(sizeof(UInt8Array));
+            Array_ctor(&ptr->cfg, UInt8, i);
+            ptr->data = (uint8*) calloc(ptr->cfg.nitem, sizeof(uint8));
+            return ptr;
+        }
+        case UInt16: {
+            UInt16Array* ptr;
+            ptr = (UInt16Array*) malloc(sizeof(UInt16Array));
+            Array_ctor(&ptr->cfg, UInt16, i);
+            ptr->data = (uint16*) calloc(ptr->cfg.nitem, sizeof(uint16));
+            return ptr;
+        }
+        case UInt32: {
+            UInt32Array* ptr;
+            ptr = (UInt32Array*) malloc(sizeof(UInt32Array));
+            Array_ctor(&ptr->cfg, UInt32, i);
+            ptr->data = (uint32*) calloc(ptr->cfg.nitem, sizeof(uint32));
+            return ptr;
+        }
+        case Int8: {
+            Int8Array* ptr;
+            ptr = (Int8Array*) malloc(sizeof(Int8Array));
+            Array_ctor(&ptr->cfg, Int8, i);
+            ptr->data = (sint8*) calloc(ptr->cfg.nitem, sizeof(sint8));
+            return ptr;
+        }
+        case Int16: {
+            Int16Array* ptr;
+            ptr = (Int16Array*) malloc(sizeof(Int16Array));
+            Array_ctor(&ptr->cfg, Int16, i);
+            ptr->data = (sint16*) calloc(ptr->cfg.nitem, sizeof(sint16));
+            return ptr;
+        }
+        case Int32: {
+            Int32Array* ptr;
+            ptr = (Int32Array*) malloc(sizeof(Int32Array));
+            Array_ctor(&ptr->cfg, Int32, i);
+            ptr->data = (sint32*) calloc(ptr->cfg.nitem, sizeof(sint32));
+            return ptr;
+        }
+        case Real32: {
+            Real32Array* ptr;
+            ptr = (Real32Array*) malloc(sizeof(Real32Array));
+            Array_ctor(&ptr->cfg, Real32, i);
+            ptr->data = (real32*) calloc(ptr->cfg.nitem, sizeof(real32));
+            return ptr;
+        }
+        default:
+            break;
     }
     
     
--- test/test_array.c
+++ test/test_array.c
@@ -11,6 +11,10 @@ void test_array_Initialization_static(void) {
     UInt8Array* aPtr = (UInt8Array*) Array_Create(UInt8, 4);
     TEST_ASSERT_EQUAL(UInt8, aPtr->cfg.dtype);
     TEST_ASSERT_EQUAL(4, aPtr->cfg.nitem);
+    TEST_ASSERT_EQUAL(1, sizeof(aPtr->data[0]));
 
+    Real32Array* bPtr = (Real32Array*) Array_Create(Real32, 8);
+    TEST_ASSERT_EQUAL(Real32, bPtr->cfg.dtype);
+    TEST_ASSERT_EQUAL(8, bPtr->cfg.nitem);
+    TEST_ASSERT_EQUAL(4, sizeof(bPtr->data[0]));
 }
-
